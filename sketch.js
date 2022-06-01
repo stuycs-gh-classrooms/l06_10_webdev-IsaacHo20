08c777ed18d61347d65285d9d32fd3d0fed508dc
Isaac Ho <69249863+IsaacHo20@users.noreply.github.com>
2022-06-01 15:14:34 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,15 @@
 function setup() {
-  // put setup code here
+  background(200);
+  size(1000, 1000);
+  i = 0;
+  options[] = {0, 200};
+  while (i <= 100) {
+    colourRect(options);
+    i++;
+  }
 }
 
 function draw() {
-  // put drawing code here
+  fill(options[int(random(2))], options[int(random(2))], options[int(random(2))]);
+  rect(int(random(800)), int(random(800)), int(random(200)), int(random(200)));
 }
