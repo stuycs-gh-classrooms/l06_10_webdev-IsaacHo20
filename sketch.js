8b19ad1958a95c856c5a64170304274abc5b71b1
Isaac Ho <69249863+IsaacHo20@users.noreply.github.com>
2022-06-01 15:20:29 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,15 +1,53 @@
+var centerx;
+var centery;
+var gx;
+var gy;
+var theta;
+var col;
+var stretch;
+var midline;
+
 function setup() {
-  background(200);
-  size(1000, 1000);
-  i = 0;
-  options[] = {0, 200};
-  while (i <= 100) {
-    colourRect(options);
-    i++;
-  }
+  frameRate(1);
+  createCanvas(500, 500);
+  centerx = width / 2;
+  centery = height / 2;
+  gx = 3 * width / 4;
+  gy = centery;
+  theta = 0;
+  col = 0;
+  stretch = width / 4;
+  midline = height / 2;
+  
+  line(centerx, centery, gx, gy);
+  circle(gx, gy, 50);
 }
 
 function draw() {
-  fill(options[int(random(2))], options[int(random(2))], options[int(random(2))]);
-  rect(int(random(800)), int(random(800)), int(random(200)), int(random(200)));
+  background(150);
+  fill(col);
+  
+  line(centerx, centery, gx, gy);
+  circle(gx, gy, 50);
+  
+  if (theta < 360) {
+    theta += 6;
+  }
+  else {
+    theta = 0;
+  }
+  col = theta * 255 / 360;
+  
+  gx = newX(stretch, centerx, theta);
+  gy = newY(stretch, centery, theta);
+}
+
+function newX(radius, centx, t) {
+  var x = radius * cos(radians(t)) + centx;
+  return x;
+}
+
+function newY(radius, centy, t) {
+  y = radius * sin(radians(t)) + centy;
+  return y;
 }
