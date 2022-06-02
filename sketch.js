b1ce2439e52339cdeb04aeaf798035a39a5db34a
Isaac Ho <69249863+IsaacHo20@users.noreply.github.com>
2022-06-02 14:55:49 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,53 +1,97 @@
-var centerx;
-var centery;
-var gx;
-var gy;
-var theta;
-var col;
-var stretch;
-var midline;
+float ghrs, gmins, gsecs;
+int centerx, centery;
+float gx, gy;
+int theta;
+int clockSize, secsHandSize, minsHandSize, hrsHandSize;
 
-function setup() {
-  frameRate(1);
-  createCanvas(500, 500);
+void setup() {
+  size(500, 500);
+  background(0);
+  fill(0);
+  
+  ghrs = hour();
+  gmins = minute();
+  gsecs = second();
   centerx = width / 2;
   centery = height / 2;
-  gx = 3 * width / 4;
-  gy = centery;
+  gx = 0.0;
+  gy = 0.0;
   theta = 0;
-  col = 0;
-  stretch = width / 4;
-  midline = height / 2;
-  
-  line(centerx, centery, gx, gy);
-  circle(gx, gy, 50);
+  clockSize = 19 * width / 20;
+  secsHandSize = 9 * clockSize / 20;
+  minsHandSize = 4 * clockSize / 10;
+  hrsHandSize = clockSize / 4;
 }
 
-function draw() {
-  background(col(150));
-  fill(col);
-  
-  line(centerx, centery, gx, gy);
-  circle(gx, gy, int(50));
-  
-  if (theta < 360) {
-    theta += 6;
-  }
-  else {
-    theta = 0;
-  }
-  col = theta * 255 / 360;
-  
-  gx = newX(stretch, centerx, theta);
-  gy = newY(stretch, centery, theta);
+void draw() {
+  background(0);
+  clockFace();
+  drawHand("hours", ghrs);
+  drawHand("minutes", gmins);
+  drawHand("seconds", gsecs);
+  updateTime();
 }
 
-function newX(radius, centx, t) {
-  var x = radius * cos(radians(t)) + centx;
+float timeToAngle(String type, float time) {
+  float t = 0;
+  
+  if(type == "hours") {
+    t = radians(time * 30 - 90);
+  }
+  else if(type == "minutes" || type == "seconds") {
+    t = radians(time * 6 - 90);
+  }
+  
+  println(type + ": " + time + ", " + + t);
+  return t;
+}
+
+void drawHand(String type, float time) {
+  //Check hw10_spin for this part
+  float t = timeToAngle(type, time);
+  
+  if(type == "hours") {
+    //draw hour hand
+    stroke(200, 200, 0);
+    strokeWeight(5);
+    line(centerx, centery, newX(hrsHandSize, centerx, t), newY(hrsHandSize, centery, t));
+  }
+  else if(type == "minutes") {
+    //draw minute hand
+    stroke(200, 100, 0);
+    strokeWeight(3);
+    line(centerx, centery, newX(minsHandSize, centerx, t), newY(minsHandSize, centery, t));
+  }
+  else if(type == "seconds") {
+    //draw second hand
+    stroke(200, 0, 0);
+    strokeWeight(1);
+    line(centerx, centery, newX(secsHandSize, centerx, t), newY(secsHandSize, centery, t));
+  }
+}
+
+void clockFace() {
+  stroke(200);
+  strokeWeight(5);
+  circle(centerx, centery, clockSize);
+  
+  drawHand("hours", ghrs);
+  drawHand("minutes", gmins);
+  drawHand("seconds", gsecs);
+}
+
+void updateTime() {
+  gsecs = second();
+  gmins = minute() + (gsecs / 60);
+  ghrs = hour() + (gmins / 60);
+}
+
+float newX(int radius, int centx, float t) {
+  float x = radius * cos(t) + centx;
   return x;
 }
 
-function newY(radius, centy, t) {
-  y = radius * sin(radians(t)) + centy;
+float newY(int radius, int centy, float t) {
+  float y = radius * sin(t) + centy;
   return y;
 }
